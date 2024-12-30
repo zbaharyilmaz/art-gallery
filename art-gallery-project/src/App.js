ce95f297299759c5bd77bc905d8910486d6b4574
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-12-30 11:41:12 +0300

    .gitignore &&deploy arrangemens completed.

--- art-gallery-project/src/App.js
+++ art-gallery-project/src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-       <h1>React First Project</h1>
-       <p>Hello React!</p>
-        
+   
     </div>
   );
 }
